4e47777bdba7804959221b689ffdcd295acc3176
yuliamar <vasya2605@yandex.ru>
2018-01-10 09:51:39 +0100

    Calender added

---
+++ app/src/main/java/com/example/yulia_000/myexpenses/CalenderActivity.java
@@ -0,0 +1,40 @@
+package com.example.yulia_000.myexpenses;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CalendarView;
+
+import com.example.yulia_000.myexpenses.R;
+
+/**
+ * Created by Yulia_000 on 08.01.2018.
+ */
+
+public class CalenderActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private CalendarView mCalenderView;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_layout);
+        mCalenderView = (CalendarView)findViewById(R.id.calendarView);
+
+        mCalenderView.setOnDateChangeListener((new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
+                String date = i2 + "/" + (i1 + 1) + "/" + i;
+                Log.d(TAG, date);
+
+                Intent intent = new Intent(CalenderActivity.this, MainActivity.class);
+                intent.putExtra("date", date);
+                startActivity(intent);
+            }
+        }));
+    }
+}
