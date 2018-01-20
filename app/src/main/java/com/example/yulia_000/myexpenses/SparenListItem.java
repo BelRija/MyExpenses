15cc3686b92c051dd65bc4dfe4c0c97772280550
Marija <sharpeima@gmail.com>
2018-01-20 16:19:16 +0100

    Ringdiagramm mit Kategorien (in Bearbeitung)

---
+++ app/src/main/java/com/example/yulia_000/myexpenses/SparenListItem.java
@@ -0,0 +1,36 @@
+package com.example.yulia_000.myexpenses;
+
+/**
+ * Created by Marija on 20.01.2018.
+ */
+
+public class SparenListItem {
+
+    private String bezeichnung;
+    private float betrag;
+
+    public SparenListItem(String bezeichnung, float betrag){
+
+        if(bezeichnung!=null && betrag!=0){
+            setBetrag( betrag );
+            setBezeichnung( bezeichnung );
+        }
+
+    }
+
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
+
+    public float getBetrag() {
+        return betrag;
+    }
+
+    public void setBetrag(float betrag) {
+        this.betrag = betrag;
+    }
+}
